3827598e645c498cae8be494efb8f5220f675bb2
Taysoniya <droopy6781@gmail.com>
2025-11-13 11:28:36 +0900

    更新

--- test.bash
+++ test.bash
@@ -4,13 +4,13 @@
 
 ng () {
 	echo ${1}行目が違うよ
-	res=1
+	res= 1
 }
 
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+["${out}"= 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+["${res}"= 0 ] && echo OK
 exit $res
