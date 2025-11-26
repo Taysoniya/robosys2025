70da755b63596ad0c6f10897245e350745864343
Taysoniya <droopy6781@gmail.com>
2025-11-26 11:50:28 +0900

    update

--- test.bash
+++ test.bash
@@ -1,16 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 # SPDX-FileCopyrightText: 2025 Obata Yuuto
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ
-	res= 1
+      echo ${1}行目が違うよ
+      res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-["${out}"= 15 ] || ng "$LINENO"
+out=$(echo 'int main(){return 0;}' | ./c-newline-add)
 
-["${res}"= 0 ] && echo OK
+echo "${out}" | grep -q 'int main(){' || ng "$LINENO"
+echo "${out}" | grep -q 'return 0;' || ng "$LINENO"
+echo "${out}" | grep -q '}' || ng "$LINENO"
+
+echo "" | ./c-newline-add > /dev/null 2>&1 || ng "$LINENO"
+
 exit $res
